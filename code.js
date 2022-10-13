0716e797039ce63837b24b63c05246eec1fdb123
Crystal <crystal.m.deleon00@gmail.com>
2022-10-13 13:53:41 -0500

    tests.js

--- code.js
+++ code.js
@@ -2,3 +2,16 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+
+
+function sayHello(str) {
+    return ""
+}
+
+
+function sayHello(x) {
+    if (x === "Jane") {
+        return "Hello, Jane"
+    }
+}
--- tests.js
+++ tests.js
@@ -3,12 +3,18 @@ describe('helloWorld', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
     });
+
+
     it('should return a string when called', function() {
       expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+
+
+    it('should return "Hello, Jane"', function() {
+      expect(sayHello("Jane")).toBe("Hello, Jane");
     });
+
+
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
